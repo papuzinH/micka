d5f02309f8b7c74f9dc11837218155d84d4d2f40
papuzinh <lautaro.hudson@ort.edu.ar>
2026-07-23 13:57:46 -0300

    feat(branding): generated favicon/app icons, drop scaffold defaults

---
+++ src/app/icon.tsx
@@ -0,0 +1,30 @@
+import { ImageResponse } from "next/og";
+import { readFile } from "fs/promises";
+import { join } from "path";
+
+export const size = { width: 64, height: 64 };
+export const contentType = "image/png";
+
+export default async function Icon() {
+  const syne = await readFile(join(process.cwd(), "src/assets/fonts/Syne-ExtraBold.ttf"));
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#212121",
+          color: "#a020f0",
+          fontFamily: "Syne",
+          fontSize: 40,
+        }}
+      >
+        M.
+      </div>
+    ),
+    { ...size, fonts: [{ name: "Syne", data: syne, weight: 800, style: "normal" }] },
+  );
+}
